92437988ef30fb601f2d3e29bf32239b24260828
Flavio Nascimento <54750750+flavinkk@users.noreply.github.com>
2022-03-30 22:12:16 -0300

    asdkgfnoasdjnfaos
    dfa

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+// Existe apenas com o intuito de organização de código.
+namespace ConsoleApp 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mizera");
+        }
+    }
+}
